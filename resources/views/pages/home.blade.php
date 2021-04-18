c021738136971a9b12770ab1bc3a60d5b11b8d78
Muhammad Arul <59349560+muhammadarl@users.noreply.github.com>
2021-04-18 14:49:57 +0700

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -132,7 +132,7 @@
         <div class="row row-kumpulan">
             <div class="col-sm-4">
                 <div class="card"">
-                    <img class="card-img-top" src="{{url('frontend/img/logo.jpg')}}" alt="Card image cap">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1jJtxbTsKMxo0CcRbZN5FpiCij2a7Sq-9" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
