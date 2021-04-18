2d6646f5ae5c3e3fd90158d3935b2ab41ceed1cf
Muhammad Arul <59349560+muhammadarl@users.noreply.github.com>
2021-04-18 19:44:53 +0700

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -131,26 +131,33 @@
         <h1 class="heading-kumpulan text-center">Personal Project</h1>
         <div class="row row-kumpulan">
             <div class="col-sm-4">
-                <div class="card"">
-                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1jJtxbTsKMxo0CcRbZN5FpiCij2a7Sq-9" alt="Card image cap">
+                <div class="card kotak-konten">
+                    <img class="card-img-top konten-gambar" src="https://drive.google.com/thumbnail?id=1jJtxbTsKMxo0CcRbZN5FpiCij2a7Sq-9" alt="Card image cap">
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h1 class="subheading-kumpulan">Omiku Universe Logo</h1>
+                        <hr>
+                        <p class="card-text content-kumpulan">Omiku Universe is a fashion company that has men's and women's 
+                            clothing products with an anime theme but is designed using a brutalism style</p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card"">
-                    <img class="card-img-top" src="https://picsum.photos/id/237/200/300" alt="Card image cap">
+                <div class="card kotak-konten">
+                    <img class="card-img-top konten-gambar" src="https://drive.google.com/thumbnail?id=1JnByX0TIeu_g7ijEHRT16gM0xZsyHAaj" alt="Card image cap">
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h1 class="subheading-kumpulan">Wisma Clothing Logo</h1>
+                        <hr>
+                        <p class="card-text content-kumpulan">Wisma Clothing is a fashion company that has men's and women's clothing product with a basic style</p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
-                <div class="card"">
-                    <img class="card-img-top" src="https://picsum.photos/id/237/200/300" alt="Card image cap">
-                    <div class="card-body bg-transparent">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div class="card kotak-konten">
+                    <img class="card-img-top konten-gambar" src="https://drive.google.com/thumbnail?id=1MGScF9kXQlmjU4phOgEUbD7IRCFioQE4" alt="Card image cap">
+                    <div class="card-body">
+                        <h1 class="subheading-kumpulan">IN Group Logo</h1>
+                        <hr>
+                        <p class="card-text content-kumpulan">IN Group merupakan grup perusahaan startup yang menggeluti di sektor teknologi</p>
                     </div>
                 </div>
             </div>
