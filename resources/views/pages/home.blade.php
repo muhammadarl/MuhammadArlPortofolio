1c6c1af86e9e0199067aaaa48cdce629b29067e4
weeklyon <syiarul45@gmail.com>
2021-04-18 14:42:29 +0700

    tambah asset

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -132,7 +132,7 @@
         <div class="row row-kumpulan">
             <div class="col-sm-4">
                 <div class="card"">
-                    <img class="card-img-top" src="https://picsum.photos/id/237/200/300" alt="Card image cap">
+                    <img class="card-img-top" src="{{url('frontend/img/logo.jpg')}}" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
